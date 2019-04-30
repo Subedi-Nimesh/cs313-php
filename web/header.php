91fae3aa3cf0edeefa5ef2bb2bacb4a637dfd03f
Nimesh Subedi <sub17003@byui.edu>
2019-04-30 16:39:02 -0600

    added files for team project.

--- web/header.php
+++ web/header.php
@@ -1,6 +1,6 @@
 <?php
     echo "<header>React</header>";
-    echo "<ul> <li> <a href="/web/login.php">Login</a></li>";
-    echo "<li> <a href="/web/home.php">Home Page</a></li>";
-    echo "<li> <a href="/web/about-us.php">About Us</a></li></ul>";
+    echo "<a href="/web/login.php">Login</a>";
+    echo "<a href="/web/home.php">Home Page</a>";
+    echo "<a href="/web/about-us.php">About Us</a>";
 ?>
