09a7cc4ede67a4c648a99ebc2786ac09631b5583
Nimesh Subedi <sub17003@byui.edu>
2019-04-30 16:25:57 -0600

    added files for team project.

---
+++ web/about-us.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php include'header.php';?>
+    <?php
+        echo "<h1>React!</h1>";
+        echo "<hr>";
+        echo "About us:<br>";
+        echo "We are a companny that creates chemical atom bomb. <br>";
+        echo "There are only two of us. and i dont think we can create the bomb in time.";
+    ?>
+</body>
+</html>
