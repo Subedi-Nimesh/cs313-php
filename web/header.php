b0b10e2baeedc27624fc7ba2a00291dde4a4359f
Nimesh Subedi <sub17003@byui.edu>
2019-04-30 22:48:31 -0600

    edited nav php

--- web/header.php
+++ web/header.php
@@ -1,6 +1,7 @@
 <?php
-    echo "<header>React</header>";
-    echo "<a href="login.php">Login</a>";
-    echo "<a href="home.php">Home Page</a>";
-    echo "<a href="about-us.php">About Us</a>";
+    <?php
+    echo '<a href="/home.php">Home</a> -
+    <a href="/about-us.php">About us</a> -
+    <a href="/login.php">Login</a>';
+    ?>
 ?>
