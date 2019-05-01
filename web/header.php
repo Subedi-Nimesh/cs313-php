ea52504d05f99e59ce1424e4d1b58ae3c67673df
Nimesh Subedi <sub17003@byui.edu>
2019-04-30 23:17:29 -0600

    edited nav php

--- web/header.php
+++ web/header.php
@@ -1,5 +1,16 @@
-<?php
-    echo '<header>React</header><a href="/home.php">Home</a> -
-    <a href="/about-us.php">About us</a> -
-    <a href="/login.php">Login</a>';
-?>
+<nav class="navbar">
+	<ul class="nav navbar-nav">
+
+		<li class="nav-item <?php if ($file === 'about-us') echo 'active' ?>">
+			<a href="about.php">About Us</a>
+		</li>
+
+		<li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
+			<a href="home.php">Home</a>
+		</li>
+
+		<li class="nav-item <?php if ($file === 'login') echo 'active' ?>">
+			<a href="login.php">Login</a>
+		</li>
+	<ul>
+</nav>
