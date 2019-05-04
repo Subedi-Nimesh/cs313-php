b8a683b022efa3ffe0706f2e738096f9ab6adf27
Nimesh Subedi <sub17003@byui.edu>
2019-05-04 12:48:29 -0600

    added homepage.

---
+++ web/homepage.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Nimesh Subedi</title>
+	</head>
+	<body style="background-image:url('bgimg.jpg'); height=100%;
+		background-position: center; background-repeat: no-repeat;
+		background-size: cover;">
+		<button onclick="window.location.href='index.html'"> &#8592; CS 213 Assignments </button>
+		<h1 style="text-align: center;"><u>Welcome to Nimesh Subedi's Home Page</u></h1>
+		<p style="text-align: center; font-family: Verdana"><button onclick="window.location.href='#about_me'"> About Me </button> <button onclick="window.location.href='#schedule'"> Course Schedule </button> <button onclick="window.location.href='https://www.facebook.com/Rajendra.cool'"> Facebook </button> <button onclick="window.location.href='https://www.instagram.com/nimesh.subedi1/?hl=en'"> Instagram </button> <button type="button" onclick = "alert('I thought I told you not to click me. Why are you STALKING me?')">Don't Click Me!</button></p>
+
+
+		<div style="text-align:center">
+			<img src="me.jpg" alt="My Picture" width="600" height="450">
+		</div>
+
+		<p><h2 id ="about_me">About me:</h2>My name is Nimesh Subedi and I'm a 20-year-old. I am <a style="color:red;"> An International Student</a> currently enrolled as a full-time student at <I>The Brigham Young University - Idaho</I> working on a <b>Bachelor's degree in Computer Science</b>
+		
+		<p>I hope to use my time at Brigham Young University - Idaho for more than getting credits and a degree.  It is important to me that I gain experience as a leader and civically engaged citizen, so that I can better myself and my community during and after my I receive my education.</p>
+
+		<p>I am originally from Nepal but found my home in the beautiful mountains of Utah. Life is about enjoying the simple things. I enjoy Family, Walking, Camping, Movies, Dinner...<br>
+		My favorite music: Mercy by Kanye West<br>
+		My favorite Holidays: Holi (Festival of colors)!!!<br><br>
+
+		My friends and family mean the world to me, and have always been a huge part of my life.</p>
+
+		<div style="text-align:center">
+			<img src="Family.jpg" alt="My Family" width="639" height="622">
+		</div>
+		<hr>
+		<blockquote style = "font-family:'Arieal'; font-style:italic;"> 
+            "If you can't explain it simply, you don't understand it well enough.<br>Two things are infinite: the universe and human stupidity; and I'm not sure about the universe." - Albert Einstein
+         </blockquote>
+		<hr>
+
+		<table style="width:97%; border-collapse: collapse; font-size: 2em; margin-left: auto; margin-right: auto;background-color: lightgrey">
+		<caption id ="schedule"><b>Course Schedule: Fall 2018</b></caption>
+		<tr style="border: 2px solid black; background-color: gray">
+			<th style="text-align: left;">Course Number</th>
+			<th style="text-align: left;">Course Description</th>
+			<th style="text-align: left;">Section Number</th>
+			<th style="text-align: left;">Class Time</th>
+			<th style="text-align: left;">Location</th>
+		</tr>
+		<tr style="border: 2px solid black">
+			<td>CS 235</td>
+			<td>Data Structures</td>
+			<td>03</td>
+			<td>MWF 10:15-11:15 PM</td>
+			<td>STC 148</td>
+		</tr>
+		<tr style="border: 2px solid black;">
+			<td>CS 213</td>
+			<td>Web Engineering I</td>
+			<td>01</td>
+			<td>TR 9:45-11:15 AM</td>
+			<td>STC 398</td>
+		</tr>
+		<tr style="border: 2px solid black">
+			<td>FDREL 327</td>
+			<td>Pearl of Great Price</td>
+			<td>04</td>
+			<td>TR 11:30-12:30 PM</td>
+			<td>TAY 275</td>
+		</tr>
+	</table>
+	
+	<p style = "text-align: right">Contact information: sub17003@byui.edu<br>&#169; Copyright</p>
+	</body>
+</html>
