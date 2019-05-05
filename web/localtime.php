334a1fc8d872f21d4f21db642b9bf57246b36b1d
Nimesh Subedi <sub17003@byui.edu>
2019-05-04 23:40:37 -0600

    adding php

--- web/localtime.php
+++ web/localtime.php
@@ -1,6 +1,6 @@
 <?php 
     date_default_timezone_set("America/Boise");
-    echo "<font size='32' face='Arial'>";
+    echo "<font size='35' face='Arial'>";
     echo "Current time on this server is " . date("h:i:sa"); 
     echo "</font>"
 ?>
