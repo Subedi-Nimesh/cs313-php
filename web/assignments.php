00321ccf8c770b9b604f565c4fb87cea5721769f
Nimesh Subedi <sub17003@byui.edu>
2019-05-04 23:08:02 -0600

    added assignment page

---
+++ web/assignments.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Nimesh Subedi's Assignments</title>
+	</head>
+	<body style="background-image:url('bground.jpg'); height=100%;
+		background-position: center; background-repeat: no-repeat;
+		background-size: cover;">
+		<h1>CS 313 Assignments</h1>
+		<ul>
+			<h2>
+                <li><a href="homepage.php">Home Page</a></li>
+    			<li><a href="assign02.html">Comming Soon</a></li>
+	    		<li><a href="assign03.html">Comming Soon</a></li>
+		    	<li><a href="assign04.html">Comming Soon</a></li>
+			    <li><a href="assign05.html">Comming Soon</a></li>
+    			<li><a href="assign06.html">Comming Soon</a></li>
+	    		<li><a href="assign07.html">Comming Soon</a></li>
+		    	<li><a href="assign08.html">Comming Soon</a></li>
+			    <li><a href="assign09.html">Comming Soon</a></li>
+    			<li><a href="assign10.html">Comming Soon</a></li>
+	    		<li><a href="assign11.html">Comming Soon</a></li>
+                <li><a href="assign12.html">Comming Soon</a></li>
+            </h2>
+		</ul>
+	</body>
+</html>
