13d7709fa0128d75909bd0dc1222696e39139bf0
Nimesh Subedi <sub17003@byui.edu>
2019-06-14 11:29:27 -0600

    Irrelevant

--- web/Project1/addanswer.php
+++ web/Project1/addanswer.php
@@ -17,26 +17,6 @@
   </script>
 </head>
 <body>
-<!-- <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-  <img src="/w3images/avatar_smoke.jpg" style="width:100%">
-  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
-    <i class="fa fa-home w3-xxlarge"></i>
-    <p>HOME</p>
-  </a>
-  <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-user w3-xxlarge"></i>
-    <p>ABOUT</p>
-  </a>
-  <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-eye w3-xxlarge"></i>
-    <p>PHOTOS</p>
-  </a>
-  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-envelope w3-xxlarge"></i>
-    <p>CONTACT</p>
-  </a>
-</nav> -->
-
 	<p><b>Add new Answers:</b><br> 
 	<form action="displayquestions.php" onsubmit='return confirmation()' method="POST">
   	<?php
--- web/Project1/displayquestions.php
+++ web/Project1/displayquestions.php
@@ -46,33 +46,17 @@ window.location="displayquestions.php?Id="+id;
 </script>
 </head>
 <body>
-<!-- <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-  <img src="/w3images/avatar_smoke.jpg" style="width:100%">
-  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
-    <i class="fa fa-home w3-xxlarge"></i>
-    <p>HOME</p>
-  </a>
-  <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-user w3-xxlarge"></i>
-    <p>ABOUT</p>
-  </a>
-  <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-eye w3-xxlarge"></i>
-    <p>PHOTOS</p>
-  </a>
-  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-envelope w3-xxlarge"></i>
-    <p>CONTACT</p>
-  </a>
-</nav> -->
 
 	<p><b>All Questions:</b><br> 
 		<?php
+    $number = 1;
 			foreach ($db->query('SELECT * FROM questions') as $row)
 			{
+        echo "$number";
           echo "<a href=\"javascript:confirmation(".$row['id'].");\">". $row['questions']. "</a>";
           echo "<button><a href='editquestion.php?Id=".$row['id']."'>Edit Question</a></button><br>";
-      }
+          $number += 1;
+        }
       
 		?>
 
