8945f1e5171d52e65a723c2644b40174f6f11c58
Nimesh Subedi <sub17003@byui.edu>
2019-06-15 09:34:47 -0600

    Irrelevant

---
+++ web/Project1/millionaire.php
@@ -0,0 +1,20 @@
+<?php
+    include'connectdatabase.php';
+    session_start();
+    $_SESSION['Answer_id'] = -1;
+    $_SESSION['Score'] = 0;
+    $_SESSION['Prev'] = array();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="game.php">Start Game</a>
+</body>
+</html>
