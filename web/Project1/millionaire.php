458dcd47d8c55493f303b9c25c1dd687afaf953a
Nimesh Subedi <sub17003@byui.edu>
2019-06-20 16:23:34 -0600

    Irrelevant

--- web/Project1/millionaire.php
+++ web/Project1/millionaire.php
@@ -13,10 +13,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Millionaire</title>
 </head>
-<body style="background-color:blue">
-<img src="logo.png";>
+<body class="body">
+<img src="logo.png" width='20px' height='20px'>
     <button class = "button" style ="margin-left:640px"><a href="game.php">Start Game</a></Button>
     <button class = "button" style ="margin-top:650px"><a href="displayquestions.php">Edit Questions</a></Button>
 </body>
